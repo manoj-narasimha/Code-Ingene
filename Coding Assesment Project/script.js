a98a7c0cdbd52d2b0f4e47c53a95f6bba59a69f6
Manoj Narasimha <79687695+manoj-narasimha@users.noreply.github.com>
2024-02-26 21:53:56 +0530

    Delete Coding Assesment Project directory

--- Coding Assesment Project/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const menuIcon = document.querySelector('.menu-icon');
-  const navList = document.querySelector('.nav-list');
-
-  menuIcon.addEventListener('click', function () {
-    navList.classList.toggle('show');
-  });
-});
